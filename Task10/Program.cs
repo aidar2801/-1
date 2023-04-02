5e13e8002275e7561601a8609ddfbf0a41c69c3d
Your Name <you@example.com>
2023-04-02 20:07:29 +0500

    написана task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondRank = number / 10 % 10;
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Число не трехзначное");
+    return;
+}
+else
+{
+Console.WriteLine($"Вторая цифра {secondRank}");
+}
